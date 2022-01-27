1abcdc92e7283b21b17cc52c9a83f162573a5ded
erik streller <erik.lilp@gmail.com>
2022-01-27 19:18:00 +0100

    add more planets and tweak responsive layout

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,6 @@
 import planet1 from "../assets/planet1.png"
+import planet2 from "../assets/planet2.png"
+import planet3 from "../assets/planet3.png"
 
 const Hero = () => {
   return (
@@ -6,20 +8,20 @@ const Hero = () => {
       id="hero"
       className="h-screen w-full top-0 flex justify-center bg-gradient-to-b from-darkblue to-dark"
     >
-      <div className="text-light text-center md:pt-36 pt-64 sm:px-[11%]">
+      <div className="text-light text-center md:pt-32 pt-[24vh] sm:px-[11%] z-10">
         <div className="sm:text-4xl text-5xl md:max-w-sm max-w-lg mb-8">
           Lorem ipsum dolor sit amet, consetetur
         </div>
         <div className="w-fit mx-auto">
           <div className="stroke bg-primary sm:mr-0 mr-14 mb-5" />
-          <div className="stroke bg-secondary sm:ml-0 ml-14 sm:absolute sm:top-28" />
+          <div className="stroke bg-secondary sm:ml-0 ml-14 sm:absolute sm:top-24" />
         </div>
         <div className="sm:text-xl text-2xl md:max-w-sm max-w-md mx-auto mt-10">
           Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
           nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
         </div>
       </div>
-      <div className="text-light absolute bottom-3 flex items-center sm:hidden">
+      <div className="text-light absolute bottom-3 flex items-center md:hidden">
         <div className="mr-4">scroll</div>
         <div className="h-20 w-0.5 bg-light rounded-full" />
         <div className="ml-4">down</div>
@@ -29,7 +31,33 @@ const Hero = () => {
         <div className="h-6 w-0.5 bg-light rounded-full ml-[6px]" />
         <div className="-rotate-90">1</div>
       </div>
-      <img src={planet1} alt="sun" className="absolute bottom-0 right-0" />
+      <img
+        src={planet1}
+        alt="sun"
+        className="fixed bottom-0 right-0
+      lg:scale-90 lg:-right-6 lg:-bottom-6 lg:opacity-95
+      md:scale-[.7] md:-right-16 md:-bottom-16 sm:opacity-90"
+      />
+      <img
+        src={planet2}
+        alt="mars"
+        className="fixed top-[20vh] right-[13vw] 
+        lg:right-20 lg:opacity-75
+        md:scale-50 md:top-14 md:right-0
+        sm:top-8 sm:-right-8 sm:scale-[.3]"
+      />
+      <img
+        src={planet3}
+        alt="moon1"
+        className="fixed bottom-[32vh] right-[25vw]
+        lg:opacity-75"
+      />
+      <img
+        src={planet3}
+        alt="moon2"
+        className="fixed bottom-[45vh] right-[9vw]
+        lg:opacity-75"
+      />
     </section>
   )
 }
