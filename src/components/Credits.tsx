37adbc639b368d88651fb7180be26352794091cf
erik streller <erik.lilp@gmail.com>
2022-02-02 23:10:34 +0100

    add credit links

--- src/components/Credits.tsx
+++ src/components/Credits.tsx
@@ -2,10 +2,20 @@ const Credits = () => {
   return (
     <p className="text-light text-xs absolute sm:left-[6.25%] left-10 bottom-10">
       Art -01
-      <a href=""> Seda</a> | -02
-      <a href=""> Efe Kurnaz </a>
+      <a href="https://www.figma.com/@seda" target="_blank">
+        {" "}
+        Seda
+      </a>{" "}
+      | -02
+      <a href="https://unsplash.com/@fakurian" target="_blank">
+        {" "}
+        Milad Fakurian{" "}
+      </a>
       {"&"}
-      <a href=""> Jr Korpa</a>
+      <a href="https://unsplash.com/@jrkorpa" target="_blank">
+        {" "}
+        Jr Korpa
+      </a>
     </p>
   )
 }
